2de2e6200317a1a84a01b3e3a2dfd0370bf25f36
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-09 10:58:26 +0200

    ++

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import React, { Component, Fragment } from 'react';
 
 import { Router } from './Router';
 
+import SearchView from './views/ASearchView';
+
+import {   TheCard1, TheCard2, TheCard3. TheCard4 } from './components/RecipeList'
+import LoadMoreList from './components/RecipeList'
+import RecipeListExample from './components/RecipeList'
+
 class App extends Component {
   render() {
     return (
