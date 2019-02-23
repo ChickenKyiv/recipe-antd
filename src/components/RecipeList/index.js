172cbe7fae2cd2434ee348d5adde56172afba094
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-24 00:01:14 +0200

    update

--- src/components/RecipeList/index.js
+++ src/components/RecipeList/index.js
@@ -4,25 +4,20 @@
 // import { TheCard1, TheCard2, TheCard3, TheCard4 } from './components/RecipeList/Cards'
 
 //@TODO rename filenames to RecipeCards....
-// import RecipeCard1 from './Cards1'
-// import RecipeCard2 from './Cards2'
-// import RecipeCard3 from './Cards3'
-// import RecipeCard4 from './Cards4'
-//
-// import RecipeCard5 from './Cards5'
 
 
 
- import {
-  RecipeCard1,
-  RecipeCard2,
-  RecipeCard3,
-  RecipeCard4,
-  RecipeCard5,
-// ClearCardsExample
-//   LoadMoreList,
-//   RecipeListExample
-} from '@groceristar/cards-wrapper'
+
+//  import {
+//   RecipeCard1,
+//   RecipeCard2,
+//   RecipeCard3,
+//   RecipeCard4,
+//   RecipeCard5,
+// // ClearCardsExample
+// //   LoadMoreList,
+// //   RecipeListExample
+// } from '@groceristar/cards-wrapper'
 
 import ClearCardsExample from  './ClearCardsExample'
 
